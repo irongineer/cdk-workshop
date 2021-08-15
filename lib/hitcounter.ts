8c3547aff3d06508ffb08dc8ac82a662a50046e4
irongineer <daichi.hotchi@gmail.com>
2021-08-15 17:35:00 +0900

    Add Assertion & Validation Tests

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -4,16 +4,26 @@ import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export interface HitCounterProps {
   downstream: lambda.IFunction;
+  readCapacity?: number;
 }
 
 export class HitCounter extends cdk.Construct {
   public readonly handler: lambda.Function;
   public readonly table: dynamodb.Table;
   constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+    if (
+      props.readCapacity !== undefined &&
+      (props.readCapacity < 5 || props.readCapacity > 20)
+    ) {
+      throw new Error('readCapacity must be greater than 5 and lower than 20');
+    }
+
     super(scope, id);
 
     const table = new dynamodb.Table(this, 'Hits', {
       partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING },
+      serverSideEncryption: true,
+      readCapacity: props.readCapacity ?? 5,
     });
     this.table = table;
 
