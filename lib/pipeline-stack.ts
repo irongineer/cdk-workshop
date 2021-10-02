ae6790004513d162489d518c06929071334f081f
irongineer <daichi.hotchi@gmail.com>
2021-10-02 17:49:50 +0900

    Add deploy stage to pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -3,6 +3,7 @@ import * as codecommit from '@aws-cdk/aws-codecommit';
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import { SimpleSynthAction, CdkPipeline } from '@aws-cdk/pipelines';
+import { WorkshopPipelineStage } from './pipeline-stage';
 export class WorkshopPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -31,5 +32,8 @@ export class WorkshopPipelineStack extends cdk.Stack {
         buildCommand: 'npm run build',
       }),
     });
+
+    const deploy = new WorkshopPipelineStage(this, 'Deploy');
+    pipeline.addApplicationStage(deploy);
   }
 }
